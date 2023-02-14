6fff4ac83e275b7e270d3410fed6b07ccc4659ba
Yura <arlosaneq@gmail.com>
2023-02-14 20:16:13 +0700

    first commit

--- app/src/main/java/com/example/secondtask_composecalculator/CalculatorButtonComponent.kt
+++ app/src/main/java/com/example/secondtask_composecalculator/CalculatorButtonComponent.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.secondtask_composecalculator.ui.theme.GoogleSans
--- app/src/main/java/com/example/secondtask_composecalculator/ui/theme/Type.kt
+++ app/src/main/java/com/example/secondtask_composecalculator/ui/theme/Type.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.secondtask_composecalculator.R
 
