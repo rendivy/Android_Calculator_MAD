ca96fa373cb073b73a3bfbf049cc8bc32298fbc6
Yura <arlosaneq@gmail.com>
2023-02-21 02:40:35 +0700

    for check

--- app/src/main/java/com/example/secondtask_composecalculator/ui/theme/Theme.kt
+++ app/src/main/java/com/example/secondtask_composecalculator/ui/theme/Theme.kt
@@ -2,17 +2,9 @@ package com.example.secondtask_composecalculator.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.secondtask_composecalculator.R
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -25,14 +17,6 @@ private val LightColorPalette = lightColors(
     primaryVariant = Purple700,
     secondary = Teal200
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 
